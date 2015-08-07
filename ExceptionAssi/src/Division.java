5eddf90432b7822f0b55243d4582b7b47fdb4d3c
alice8347 <alice8347@hotmail.com>
2015-08-07 11:56:15 -0400

    Second Commit

--- ExceptionAssi/src/Division.java
+++ ExceptionAssi/src/Division.java
@@ -19,5 +19,9 @@ class Division {
 		} catch (ArithmeticException e) {
 			System.out.println("The second integer cannot be 0.");
 		}
+		
+		finally {	// No matter the exception is caught or not, the statement in finally will always run.
+		      System.out.println("finally block will execute.");
+		}
 	}
 }
